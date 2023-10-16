21840329ad5df6632af2b27301dcda980be02823
sanjayprajapati <sanjay.prajapati80@gmail.com>
2023-10-16 11:20:14 +0530

    resolve some console issues

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -106,7 +106,7 @@ const Home = () => {
                           <ul className="flex">
                             <li>
                               <a href="#" target="_blank">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="19" height="34" viewBox="0 0 19 34" className="h-7 w-7"><g fill="none" fill-rule="evenodd"><path d="M0 0h19v34H0z"></path><path fill="#3E6FA7" stroke="#3E6FA7" stroke-width="1.393" d="M12.074 33H6.23l-.133-14.297H1V13.13h5.102l-.005-4.52C6.096 4.02 9.3 1 14.16 1l3.84.23v5.296h-3.394c-1.892 0-2.532 1.342-2.532 2.81v3.795h5.666l-.765 5.572h-4.9V33Z"></path></g></svg>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="19" height="34" viewBox="0 0 19 34" className="h-7 w-7"><g fill="none" fillRule="evenodd"><path d="M0 0h19v34H0z"></path><path fill="#3E6FA7" stroke="#3E6FA7" strokeWidth="1.393" d="M12.074 33H6.23l-.133-14.297H1V13.13h5.102l-.005-4.52C6.096 4.02 9.3 1 14.16 1l3.84.23v5.296h-3.394c-1.892 0-2.532 1.342-2.532 2.81v3.795h5.666l-.765 5.572h-4.9V33Z"></path></g></svg>
                               </a>
                             </li>
                             <li>
